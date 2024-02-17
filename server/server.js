ac924bc1e64cc335b38caf516cc5fbc951a08199
BigTzu <71331403+Mwami42@users.noreply.github.com>
2024-02-17 07:10:40 +0100

    ADD Now using Google Datastore for data handling

--- server/server.js
+++ server/server.js
@@ -1,23 +1,18 @@
 import express from "express";
 import fetch from "node-fetch";
 import cron from "node-cron";
-import fs from "fs";
-import path from "path";
 import cors from "cors";
+import { Datastore } from "@google-cloud/datastore";
 
 const app = express();
-const PORT = process.env.PORT || 8080; // 8080 as a default value if process.env.PORT is not set
+const PORT = process.env.PORT || 8080;
 const TLE_URL =
   "https://celestrak.org/NORAD/elements/gp.php?CATNR=25544&FORMAT=tle";
-const TLE_FILE_PATH = path.join("./data", "localTLEData.txt");
+const datastore = new Datastore();
 
-// CORS configuration
-const corsOptions = {
-  origin: "http://localhost:5173",
-  // For production, replace with client's domain
-};
+app.use(cors({ origin: "http://localhost:5173" })); // Update for production
 
-app.use(cors(corsOptions));
+const TLE_KIND = "TLEData";
 
 const fetchAndSaveTLE = async () => {
   const response = await fetch(TLE_URL);
@@ -25,39 +20,41 @@ const fetchAndSaveTLE = async () => {
   const tleLines = data
     .trim()
     .split("\n")
-    .map((line) => line.trim()); // Trim each line
+    .map((line) => line.trim());
+
   if (tleLines.length >= 3) {
     const tleData = {
-      line1: tleLines[0],
-      line2: tleLines[1],
-      line3: tleLines[2],
+      line1: tleLines[1],
+      line2: tleLines[2],
       date: new Date().toISOString().slice(0, 10),
     };
-    fs.writeFileSync(TLE_FILE_PATH, JSON.stringify(tleData), "utf8");
-    console.log("TLE data updated and saved.");
+
+    const entity = {
+      key: datastore.key([TLE_KIND, "latest"]),
+      data: tleData,
+    };
+
+    await datastore.save(entity);
+    console.log("TLE data updated and saved to Datastore.");
     return tleData;
+  } else {
+    throw new Error("Invalid TLE data format");
   }
-  throw new Error("Invalid TLE data format");
 };
 
-// Scheduled task to fetch TLE data once a day at 2 AM
 cron.schedule("0 2 * * *", fetchAndSaveTLE);
 
 app.get("/api/tle", async (req, res) => {
   try {
-    if (!fs.existsSync(TLE_FILE_PATH)) {
-      const tleData = await fetchAndSaveTLE();
-      res.json(tleData);
+    const key = datastore.key([TLE_KIND, "latest"]);
+    const [entity] = await datastore.get(key);
+
+    if (entity && entity.date === new Date().toISOString().slice(0, 10)) {
+      res.json(entity);
+      console.log("TLE data fetched from Datastore.");
     } else {
-      const tleData = JSON.parse(fs.readFileSync(TLE_FILE_PATH, "utf8"));
-      const today = new Date().toISOString().slice(0, 10);
-      if (tleData.date === today) {
-        res.json(tleData);
-        console.log("Already Fetched");
-      } else {
-        const updatedTleData = await fetchAndSaveTLE();
-        res.json(updatedTleData);
-      }
+      const updatedTleData = await fetchAndSaveTLE();
+      res.json(updatedTleData);
     }
   } catch (err) {
     res
@@ -70,7 +67,6 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to the ISS Tracker API" });
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
